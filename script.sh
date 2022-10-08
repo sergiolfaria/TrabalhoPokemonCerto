c4131a1af12995a38882de67c694abef84fe72c4
sergiolfaria <srgion@yahoo.com.br>
2022-10-08 01:32:18 -0300

    while incluso

--- script.sh
+++ script.sh
@@ -6,6 +6,15 @@ echo "Digite o primeiro Pokemon:"
     read poke1
 echo "Digite o segundo Pokemon:"
     read poke2
+    while [ $poke1 = $poke2 ]; do
+
+    echo "Seus pokemons são iguais, Que tipo de batalha é essa?\n ela acabaria em empate antes mesmo de começar"
+    
+    echo "Digite o primeiro Pokemon Novamente:" 
+    read poke1
+    echo "Digite o segundo Pokemon Novamente: " 
+    read poke2
+done
 echo "pokemon 1 escolhido foi:" $poke1
 echo "pokemon 2 escolhido foi:" $poke2
 grep $poke1  pkmStats.txt >>pkm1.txt
