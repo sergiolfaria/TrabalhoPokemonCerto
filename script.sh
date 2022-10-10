02cea57c2f82a910e1cca4ae7de8beaa4b4fd643
sergiolfaria <srgion@yahoo.com.br>
2022-10-09 21:42:44 -0300

    VERSÃO MELHOR

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@
 rm pkm1.txt pkm2.txt
 
 # inicio da interação com "jogador"
-echo "=-=-= BEM VINDO AO POKEBASH =-=-="
+echo "══════════BEM VINDO AO POKEBASH ══════════"
 touch pkm1.txt pkm2.txt
 echo "Digite o primeiro Pokemon:"
 read poke1
@@ -27,14 +27,12 @@ done
 echo "pokemon 1 escolhido foi:" $poke1
 echo "pokemon 2 escolhido foi:" $poke2
 echo "Boa Escolha Treinador ! Agora iremos Conferir os POKESTATS"
-printf "\n|--------------------------------------------------------------------------------------------------------------------------------|\n"
-# forma de pegar as informações nescessarias para entregar o resultado esperado do primeiro pokemon
 var_info_poke=$(grep -i $poke1 ./pkm.txt)
 status_poke=$(grep -i $poke1 ./pkmStats.txt)
 read -a info_poke <<<"$var_info_poke"
 read -a status_geral <<<"$status_poke"
 # escreve no arquivo pkm1.txt e imprime na tela
-sumario_poke_1="Numero:_${info_poke[0]}_Nome:_${info_poke[1]}_HP:_${status_geral[3]}_Ataque:_${status_geral[4]}_Velocidade:_${status_geral[8]}_Tipo:_${info_poke[2]},${info_poke[3]}_Fraquesas:_}"
+sumario_poke_1="Nome:⠀${status_geral[1]}/Numero:⠀${info_poke[0]}/Nome:⠀${info_poke[1]}/HP:⠀${status_geral[3]}/Ataque:⠀${status_geral[4]}/Velocidade:⠀${status_geral[8]}/Tipo:⠀${info_poke[2]},${info_poke[3]}⠀Fraquesas:"
 printf " "$sumario_poke_1 >>pkm1.txt
 #while que resolve as fraquesas dos pokemons
 while read desvant; do
@@ -43,14 +41,14 @@ while read desvant; do
 
         read -a atributos_poke_1 <<<"$desvant"
         if [ ${atributos_poke_1[0]} != ${info_poke[2]} ]; then
-            printf "${atributos_poke_1[0]},_" >>pkm1.txt
+            printf "${atributos_poke_1[0]},⠀" >>pkm1.txt
         fi
     else
         if [ ${#info_poke[@]} == 4 ]; then
             if echo "$desvant" | grep -q "${info_poke[3]}"; then
                 read -a atributos_poke_duo <<<"$desvant"
                 if [ ${atributos_poke_duo[0]} != ${info_poke[3]} ]; then
-                    printf "${atributos_poke_duo[0]},_" >>pkm1.txt
+                    printf "${atributos_poke_duo[0]},⠀" >>pkm1.txt
                 fi
             fi
         fi
@@ -58,9 +56,6 @@ while read desvant; do
     fi
 
 done <pkmAdv.txt
-
-cat pkm1.txt
-
 
 
 # forma de pegar as informações nescessarias para entregar o resultado esperado do segundo pokemon
@@ -70,7 +65,7 @@ read -a info_poke_2 <<<"$var_info_poke_2"
 read -a status_geral_2 <<<"$status_poke_2"
 
 # escreve no arquivo pkm2.txt e imprime na tela
-sumario_poke_2="Numero:_${info_poke_2[0]}_Nome:_${info_poke_2[1]}_HP:_${status_geral_2[3]}_Ataque:_${status_geral_2[4]}_Velocidade:_${status_geral_2[8]}_Tipo:_${info_poke_2[2]},${info_poke_2[3]}_Fraquesas:_}"
+sumario_poke_2="Nome:⠀${status_geral_2[1]}⠀/Numero:⠀${info_poke_2[0]}⠀/Nome:⠀${info_poke_2[1]}⠀/HP:⠀${status_geral_2[3]}⠀/Ataque:⠀${status_geral_2[4]}⠀/Velocidade:⠀${status_geral_2[8]}⠀/Tipo:⠀${info_poke_2[2]},${info_poke_2[3]}⠀Fraquesas:"
 printf " "$sumario_poke_2 >>pkm2.txt
 while read desvant_2; do
 
@@ -78,14 +73,14 @@ while read desvant_2; do
 
         read -a atributos_poke_2 <<<"$desvant_2"
         if [ ${atributos_poke_2[0]} != ${info_poke_2[2]} ]; then
-            printf "${atributos_poke_2[0]},_" >>pkm2.txt
+            printf "${atributos_poke_2[0]}," >>pkm2.txt
         fi
     else
         if [ ${#info_poke_2[@]} == 4 ]; then
             if echo "$desvant_2" | grep -q "${info_poke_2[3]}"; then
                 read -a atributos_poke_duo_2 <<<"$desvant_2"
                 if [ ${atributos_poke_duo_2[0]} != ${info_poke_2[3]} ]; then
-                    printf "${atributos_poke_duo_2[0]},_" >>pkm2.txt
+                    printf "${atributos_poke_duo_2[0]}," >>pkm2.txt
                 fi
             fi
         fi
@@ -93,6 +88,8 @@ while read desvant_2; do
     fi
 
 done <pkmAdv.txt
-printf "\n|--------------------------------------------------------------------------------------------------------------------------------|\n"
+printf "\n╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗\n"
+cat pkm1.txt
+printf "\n"
 cat pkm2.txt
-printf "\n|--------------------------------------------------------------------------------------------------------------------------------|\n"
+printf "\n╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝"
